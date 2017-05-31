41486ec4ccaceae711e94c6ccf2f841ce4cb92ab
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-31 00:09:35 -0700

    add onSubmit functionality for new post page to update API with new post.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,26 @@
 import axios from 'axios';
+import { API_KEY } from '../config/config'
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
+
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts?key=${window.API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts?key=${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts?key=${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
