54c452736a7fe2f4e85e66a71771d7ac6353eb69
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-31 00:15:29 -0700

    handle async posting issue for redirect on post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -25,9 +25,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(this.props)
-    console.log(values)
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
